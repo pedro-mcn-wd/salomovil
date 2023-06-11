02177c99e6336c58efc3bfbea214fd33a8576379
Pedro <cppcarnun854@ieshnosmachado.org>
2023-06-11 18:19:02 +0200

    fix

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -21,7 +21,7 @@
                                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
                                     </svg>
-                                    <span class="ml-2">{{ __('Vaciar cesta') }}</span>
+                                    <span class="ml-2 whitespace-nowrap">{{ __('Vaciar cesta') }}</span>
                                 </a>
                             </th>
                         </tr>
@@ -132,7 +132,7 @@
                             <td class="text-center">
                                 <a href="{{ route('cart.payCart') }}" class="text-white bg-green-600 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm pl-2 pr-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                    <span class="ml-2">{{ __('HACER PEDIDO') }}</span>
+                                    <span class="ml-2 whitespace-nowrap">{{ __('HACER PEDIDO') }}</span>
                                 </a>
                             </td>
                         </tr>
@@ -369,7 +369,7 @@
                             </tr>
                         @endforeach
                     </tbody>
-                    <caption class="md:hidden caption-bottom">
+                    <caption class="lg:hidden caption-bottom">
                         <div class="bg-blue-200 p-3 flex items-center justify-between">
                             <span class="font-bold text-gray-800">{{ __('TOTAL: ') }}&nbsp;{{ Cart::total() }}{{ __('€') }}</span>
                             <a href="{{ route('cart.payCart') }}" class="text-white bg-green-600 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm pl-2 pr-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
